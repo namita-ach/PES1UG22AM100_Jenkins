ce91f4ded2480c605921eb0a3ab4418f02257068
namita-ach <namita.ach04@gmail.com>
2025-03-12 11:55:42 +0530

    this is a commit message

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World. This is proof of me finishing lab. \n";
+  cout << "Hello, Jenkins. My SRN is PES1UG22AM100 \n";
+  cout << "I have successfully built and run this. I am Namita.   \n";
+  return 0;
+}
