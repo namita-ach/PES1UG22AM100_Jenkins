fe9d493806396e80a85bdc9adb2a26749a27a4d3
namita-ach <namita.ach04@gmail.com>
2025-03-12 13:37:04 +0530

    this is a commit message for the webhook part

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World. This is proof of me finishing lab. \n";
-  cout << "Hello, Jenkins. My SRN is PES1UG22AM100 \n";
+  cout << "Hello, Jenkins. My SRN is PES1UG22AM100. THIS IS CHANGED AFTER WEBHOOK. \n";
   cout << "I have successfully built and run this. I am Namita.   \n";
   return 0;
 }
